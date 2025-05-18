b4fdcd08b48fa8802026652459bda97dcf3d00c8
PHYD0 <boudaradam@gmail.com>
2025-05-18 17:36:57 +0100

    updated website

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Welcome to Adam's portfolio!");
+const easterEgg = `
+%c
+      ___           ___           ___     
+     /\\  \\         /\\__\\         /\\  \\    
+    /::\\  \\       /:/  /        /::\\  \\   
+   /:/\\:\\  \\     /:/__/        /:/\\:\\  \\  
+  /::\\~\\:\\  \\   /::\\__\\____   /::\\~\\:\\  \\ 
+ /:/\\:\\ \\:\\__\\ /:/\\:::::\\__\\ /:/\\:\\ \\:\\__\\
+ \\/_|::\\/:/  / \\/_|:|~~|~    \\:\\~\\:\\ \\/__/
+    |:|::/  /     |:|  |       \\:\\ \\:\\__\\  
+    |:|\\/__/      |:|  |        \\:\\ \\/__/  
+    |:|  |        |:|  |         \\:\\__\\    
+     \\|__|         \\|__|          \\/__/    
+
+Welcome to the console, hacker 👨‍💻
+Email me at boudaradam@gmail.com if you want to build something cool together!
+`;
+
+console.log(easterEgg, "color: #00bcd4; font-weight: bold; font-family: monospace;");
